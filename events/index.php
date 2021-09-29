138f9ffbad9dd8dddc644a2bafb9761e25d734f4
Akhila Thota <akhilacthota@gmail.com>
2021-09-29 18:56:26 +0530

    events

---
+++ events/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+    .whatsapp_float{
+        position:fixed;
+        bottom:40px;
+        right:20px;
+    }
+    </style>
+    <div class="whatsapp_float">
+        <a href="https://wa.me/919540608104" target="_blank"><img src='whatsapp.png' width="50px" class="whatsapp_float_btn"/></a>
+    </div>
+</body>
+</html>
